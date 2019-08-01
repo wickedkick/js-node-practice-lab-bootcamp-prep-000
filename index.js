a69c3722dec1f2741991c9ade72ba95def86afd0
Imran Iqbal <imran1337@gmail.com>
2019-08-01 00:30:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 var name = "Joe"
 var height = "74"
 
+// string name then calls variable name
 console.log("Name:", name)
 // console.log(name)
 
+// string name then calls variable name
 console.log("Height:", height)
 // console.log(height)
